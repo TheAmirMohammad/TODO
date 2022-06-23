fe02b4ae77695ab8d361b90ceb25e72d5ed03309
Amir Mohammad <sohrabi2557@gmail.com>
2022-06-23 05:46:45 +0430

    feat: add todoList model and admin

--- todoList/todoItem/models.py
+++ todoList/todoItem/models.py
@@ -1,3 +1,21 @@
+from turtle import title
+from venv import create
 from django.db import models
+import uuid
 
 # Create your models here.
+class todoItem(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    owner = models.OneToOneField('authApp.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    description = models.TextField(default='', blank=True, null=True)
+    isFinished = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    @property
+    def ownerUsername(self):
+        return self.owner.username

dc5b4fc3fc3368f2350e80f47cac41e6dafce822
Amir Mohammad <sohrabi2557@gmail.com>
2022-06-23 05:46:45 +0430

    feat: add todoList model and admin

--- todoList/todoItem/admin.py
+++ todoList/todoItem/admin.py
@@ -1,3 +1,17 @@
+from turtle import title
 from django.contrib import admin
+from .models import todoItem
 
 # Register your models here.
+class todoListAdminConfig(admin.ModelAdmin):
+    model = todoItem
+    search_fields = ('id', 'title')
+    list_filter = ('isFinished',)
+    ordering = ('-modified',)
+    list_display = ('title', 'ownerUsername', 'isFinished')
+    # fieldsets = (
+    #     (None, {'fields': ('id', 'title')})
+    # )
+    # fields: Optional[_FieldGroups]
+
+admin.site.register(todoItem, todoListAdminConfig)

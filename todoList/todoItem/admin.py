23fde1149cb9e0b5b5772e1468df257aa494e701
Amir Mohammad <sohrabi2557@gmail.com>
2022-06-23 12:00:18 +0430

    faet: add folder model and admin

--- todoList/todoItem/admin.py
+++ todoList/todoItem/admin.py
@@ -1,8 +1,14 @@
 from turtle import title
 from django.contrib import admin
-from .models import todoItem, tag
+from .models import todoItem, tag, folder
 
 # Register your models here.
+class folderAdminConfig(admin.ModelAdmin):
+    model = tag
+    search_fields = ('id', 'name')
+    ordering = ('-modified',)
+    list_display = ('name', 'ownerUsername')
+
 class tagAdminConfig(admin.ModelAdmin):
     model = tag
     search_fields = ('id', 'name')
@@ -14,11 +20,12 @@ class todoListAdminConfig(admin.ModelAdmin):
     search_fields = ('id', 'title')
     list_filter = ('isFinished',)
     ordering = ('-modified',)
-    list_display = ('title', 'ownerUsername', 'isFinished')
+    list_display = ('title', 'ownerUsername', 'folderName', 'tagCount', 'isFinished')
     # fieldsets = (
     #     (None, {'fields': ('id', 'title')})
     # )
     # fields: Optional[_FieldGroups]
 
+admin.site.register(folder, folderAdminConfig)
 admin.site.register(tag, tagAdminConfig)
 admin.site.register(todoItem, todoListAdminConfig)
--- todoList/todoItem/models.py
+++ todoList/todoItem/models.py
@@ -4,6 +4,20 @@ from django.db import models
 import uuid
 
 # Create your models here.
+class folder(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    owner = models.OneToOneField('authApp.User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=250, unique=True)
+    description = models.TextField(default='', blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    @property
+    def ownerUsername(self):
+        return self.owner.username
 
 class tag(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
@@ -29,6 +43,7 @@ class todoItem(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     tags = models.ManyToManyField(tag, blank=True)
+    folder = models.OneToOneField(folder, on_delete=models.SET_DEFAULT, default=None, null=True, blank=True)
 
     def __str__(self) -> str:
         return self.title
@@ -36,3 +51,13 @@ class todoItem(models.Model):
     @property
     def ownerUsername(self):
         return self.owner.username
+
+    @property
+    def folderName(self):
+        if self.folder:
+            return self.folder.name
+        return "NO FOLDER!"
+
+    @property
+    def tagCount(self):
+        return self.tags.count

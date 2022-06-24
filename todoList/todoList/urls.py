07ca7cf86004dbc63ced4403fb885615f1a82e25
Amir Mohammad <sohrabi2557@gmail.com>
2022-06-24 12:40:09 +0430

    feat: add static files settings

--- todoList/todoList/urls.py
+++ todoList/todoList/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
@@ -27,3 +29,5 @@ urlpatterns = [
     url(r'^auth/', include('djoser.urls.jwt')),
     url(r'^auth/', include('djoser.urls.authtoken')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
